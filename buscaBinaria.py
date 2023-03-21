d294689955532c642766ee321b95a4ab98660302
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-03-21 09:48:21 -0300

    busca binária

---
+++ buscaBinaria.py
@@ -0,0 +1,16 @@
+def executar_busca_binaria(lista, valor):
+    minimo = 0
+    maximo = len(lista) - 1
+
+    while minimo <= maximo:
+        # Encontra o elemento que divide a lista ao meio
+        meio = (minimo + maximo) // 2
+        # Verifica se o valor procurado está a esquerda ou direita do valor central
+        if valor < lista[meio]:
+            maximo = meio - 1
+        elif valor > lista[meio]:
+            minimo = meio + 1
+        else:
+            return True  # Se o valor dor encontrado para aqui
+
+        return False  # Se chegar até aqui, siginifica que o valor não foi encontrado
